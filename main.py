fdb0248073f7d52e60f98271ed8d72f54ca51aff
whybhav360 <vmmadaan51@gmail.com>
2025-05-13 00:52:44 +0530

    Added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model import forecast_country, generate_forecast_image, country_series, df
 import uvicorn
+import pandas as pd
 
 app = FastAPI()
 
